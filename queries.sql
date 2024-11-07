bebb655f48d350f3026330b34f1f2d38c2e52187
arunastanley <arunapaulus@gmail.com>
2024-11-07 12:53:03 -0600

    Task4 almost done- SQL query  pending

--- queries.sql
+++ queries.sql
@@ -18,7 +18,6 @@ DROP table job;
 
 --Part 4
 SELECT * FROM skill
-LEFT JOIN job_skills ON(skill.id=job_skills.skills_id)
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
 WHERE job_skills.jobs_id IS NOT NULL
-ORDER BY name ASC;
-
+ORDER BY skill.name ASC;
