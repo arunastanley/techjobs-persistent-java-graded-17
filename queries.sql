77d6d8192889565d9ea2fb1d4f269611ab4920b4
arunastanley <arunapaulus@gmail.com>
2024-11-07 18:49:06 -0600

    Task4 done

--- queries.sql
+++ queries.sql
@@ -18,6 +18,6 @@ DROP table job;
 
 --Part 4
 SELECT * FROM skill
-left JOIN job_skills ON skill.id = job_skills.skills_id
+INNER JOIN job_skills ON skill.id = job_skills.skills_id
 WHERE job_skills.jobs_id IS NOT NULL
 ORDER BY name ASC;
